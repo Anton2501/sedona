692aeef5085a8ce7140563ad273aebb13da0e0e8
Anton Prokopenko <anton.9090@bk.ru>
2018-01-31 23:38:09 +0300

    edit tools & scripts

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,11 @@
-//= ../../../bower_components/
+//= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/jquery-plugin/jquery.plugin.min.js
+//= ../../../bower_components/jquery-datepick/jquery.datepick.min.js
+//= ../../../bower_components/jquery-datepick/jquery.datepick-ru.js
+
+//= blocks/dropdown-header.js
+//= blocks/mob-menu.js
+//= blocks/booking-popup.js
+
+$('#datepicker').datepick({dateFormat: 'dd MM yyyy'});
+$('#datepicker2').datepick({dateFormat: 'dd MM yyyy'});
